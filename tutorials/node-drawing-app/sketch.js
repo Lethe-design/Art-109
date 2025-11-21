edcc2a7afefd288b6e55d1b1a04938978742a1ff
Lethe-design <ashanty.lemus@sjsu.edu>
2025-11-21 00:11:41 -0800

    added 3 files

--- tutorials/node-drawing-app/sketch.js
+++ tutorials/node-drawing-app/sketch.js
@@ -1,7 +1,35 @@
-function setup() {
+var socket;
 
+function setup() {
+    createCanvas(200, 200);
+    background(51);
+
+    socket = io.connect('http://localhost:3000')
+    socket.on('mouse', newDrawing);
+}
+
+function newDrawing(data) {
+    noStroke();
+    fill(255, 0, 100);
+    ellipse(mouseX, mouseY, 36, 36);
+}
+
+function mouseDragged() {
+    console.log('Sending: ' + mouseX + ',' + mouseY);
+
+    var data = {
+        x: mouseX,
+        y: mouseY
+    }
+
+    socket.emit('mouse', data);
+
+     noStroke();
+    fill(255);
+    ellipse(mouseX, mouseY, 36, 36);
+    
 }
 
 function draw() {
- 
+   
 }
